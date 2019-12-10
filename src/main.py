c8b21cd3c049d42fe2732f3be5844502fe9afe36
Amelia Maier <AmeliaMaier@users.noreply.github.com>
2019-12-10 11:23:26 -0700

    added tranformed columns to source tables
    added tranformed columns to source tables

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ def main():
     """
     csv_file_path = 'data_files/'
     connection = getConnection('CSV', dbname=csv_file_path)
+    # load_csvs can happen in any order
     load_csv('movies_metadata.csv', 'schema', 'src_movies_metadata', csv_file_path, connection)
     load_csv('credits.csv', 'schema', 'src_credits', csv_file_path, connection)
     load_csv('keywords.csv', 'schema', 'src_keywords', csv_file_path, connection)
@@ -20,6 +21,30 @@ def main():
     # leaving these out under assumption they are subset of larger files.
     # load_csv('links_small.csv', 'schema', 'src_links', csv_file_path, connection)
     # load_csv('ratings_small.csv', 'schema', 'src_ratings', csv_file_path, connection)
+
+    # step 1 in load_tables
+    load_table('movies')
+    load_table('collections')
+    load_table('genres')
+    load_table('production_companies')
+    # these two not needed because international standards should be filled in already
+    # load_table('production_countries') 
+    # load_table('spoken_languages')
+    load_table('keywords')
+    load_table('cast_members')
+    load_table('crew_members')
+    # step 2 in load_tables
+    load_table('movie_collections')
+    load_table('movie_genres')
+    load_table('movie_production_companies')
+    load_table('movie_production_countries')
+    load_table('movie_spoken_languages')
+    load_table('movie_keywords')
+    load_table('movie_ratings')
+    load_table('movie_cast_members')
+    load_table('movie_crew_members')
+    load_table('movie_links')
+
 
 def load_csv(csv_name, schema, table_name, file_path, connection):
     """
@@ -52,8 +77,9 @@ def load_csv(csv_name, schema, table_name, file_path, connection):
             file_info = connection.append_to_table_return_ids(file_info, schema, table_name, 'id')
             # write out file data
             data['file_id'] = file_info['id'].iloc[-1]
-            data['created_date'] = datetime.now() # this would be defaulted if writing directly to psql
-            data['transformed'] = False # this would be defaulted if writing directly to psql
+            # this section would be defaulted in the database in a real system
+            data['created_date'] = datetime.now() 
+            data = _get_transformed_columns(data, table_name) 
             connection.append_to_table(data, schema, table_name)
         else:
             raise RuntimeError(f'Main.load_csv: Table name [{table_name}] in schema [{schema}] not found. Injestions of file [{csv_name}] at [{file_path}] stopped.')
@@ -61,10 +87,42 @@ def load_csv(csv_name, schema, table_name, file_path, connection):
         raise RuntimeError("Main.load_csv: {err}")
 
 
-def load_table():
+def load_table(schema, table_name, connection):
+    # load data mapping for given table
+    # load source data for new table
+    # basic data cleaning (casting to correct type)
+    # write out data
+    # if type delete, check for returned conflicts
+    # set source data as transformed
     pass
 def load_report():
     pass
+
+def _get_transformed_columns(df, table_name):
+    if table_name == 'src_credits':
+        df['transformed_cast_members'] = False
+        df['transformed_crew_members'] = False
+        df['transformed_movie_cast_members'] = False
+        df['transformed_movie_crew_members'] = False
+    elif table_name == 'src_keywords':
+        df['transformed_keywords'] = False
+        df['transformed_movie_keywords'] = False
+    elif table_name == 'src_links':
+        df['transformed_movie_links'] = False
+    elif table_name == 'src_ratings':
+        df['transformed_movie_ratings'] = False
+    elif table_name == 'src_movie_metadata':
+        df['transformed_movies'] = False
+        df['transformed_collections'] = False
+        df['transformed_genres'] = False
+        df['transformed_production_companies'] = False
+        df['transformed_movie_collections'] = False
+        df['transformed_movie_genres'] = False
+        df['transformed_movie_production_companies'] = False
+        df['transformed_movie_production_contries'] = False
+        df['transformed_movie_spoken_languages'] = False
+    return df
+
 
 if __name__ == '__main__':
     main()
